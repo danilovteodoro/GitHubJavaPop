7f174ae234ba46286b1447ea13c28bd075e6844c
daniloteodoro <danilovteodoro@gmail.com>
2021-02-16 14:58:37 -0300

    - Creating ViewModel

--- app/src/main/java/dev/danilovteodoro/javapop/ui/viewmodel/GitRepoViewHolder.kt
+++ app/src/main/java/dev/danilovteodoro/javapop/ui/viewmodel/GitRepoViewHolder.kt
@@ -1,4 +1,38 @@
 package dev.danilovteodoro.javapop.ui.viewmodel
 
-class GitRepoViewHolder {
+import androidx.lifecycle.*
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.danilovteodoro.javapop.model.Repository
+import dev.danilovteodoro.javapop.repository.GitRepoRepository
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import util.DataState
+import javax.inject.Inject
+
+@HiltViewModel
+class GitRepoViewHolder @Inject constructor(
+    private val savedStateHandle: SavedStateHandle,
+    private val repository: GitRepoRepository
+):ViewModel(){
+    private var _repositoriesLv:MutableLiveData<DataState<List<Repository>>> = MutableLiveData()
+    val repositoriesLv:LiveData<DataState<List<Repository>>> get() = _repositoriesLv
+
+    private fun getGitRepos(){
+        repository.getRepositories().onEach { dataState ->
+            _repositoriesLv.value = dataState
+        }.launchIn(viewModelScope)
+    }
+
+    fun callAction(action:GitRepoActions){
+        when(action){
+            is GitRepoActions.GetRepos ->{
+                getGitRepos()
+            }
+        }
+    }
+
+}
+
+sealed class GitRepoActions {
+    object GetRepos : GitRepoActions()
 }
